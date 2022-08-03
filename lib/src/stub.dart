e82e7059d7d3ec0daa290e8e0b87ff5beb41be4e
Sergio Lisan <sergiolisan@gmail.com>
2022-08-03 04:08:39 -0300

    adaptado para web

--- lib/src/stub.dart
+++ lib/src/stub.dart
@@ -2,14 +2,29 @@
 ///
 library windows_pos_printer.manager.stub;
 
-import 'package:windows_pos_printer/src/manager/abstract.dart';
-
+import 'capability.dart';
 import 'enums.dart';
+import 'manager/abstract.dart';
+import 'model.dart';
 
 ///
 ///
 ///
 class WindowsPOSManager extends PrinterManager {
+  ///
+  WindowsPOSManager(
+    POSPrinter printer,
+    int paperSizeWidthMM,
+    int maxPerLine,
+    CapabilityProfile profile, {
+    int spaceBetweenRows = 5,
+    int port = 9100,
+  });
+
+  ///
+  /// [discover] let you explore all netWork printer in your network
+  static Future<List<USBPrinter>> discover() async => [];
+
   @override
   Future<ConnectionResponse> connect({Duration? timeout}) {
     throw UnimplementedError();
