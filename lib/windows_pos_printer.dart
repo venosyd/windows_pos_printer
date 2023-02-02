779ed590fdb027221c7afc371ccd30e7df25895c
Sergio Eduardo Lisan <sergiolisan@gmail.com>
2023-02-01 22:46:05 -0300

    Correcao

--- lib/windows_pos_printer.dart
+++ lib/windows_pos_printer.dart
@@ -4,5 +4,5 @@ library windows_pos_printer;
 
 export 'src/capability.dart';
 export 'src/enums.dart';
-export 'src/stub.dart' if (dart.io.library) 'src/manager/implementation.dart';
+export 'src/stub.dart' if (dart.library.io) 'src/manager/implementation.dart';
 export 'src/model.dart';
